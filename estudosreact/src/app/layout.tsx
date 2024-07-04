927b0536b5edf3c3ffbb1be475764a941a80d84d
GabrieleFerreira <gabriele.ferreira@ufvjm.edu.br>
2024-07-03 23:20:40 -0300

    adicionando imagens a aplicação

--- estudosreact/src/app/layout.tsx
+++ estudosreact/src/app/layout.tsx
@@ -7,7 +7,7 @@ export default function RootLayout({
 }>) {
   return (
     <html>
-      <body className="h-full">{children}</body>
+      <body className="h-screen  bg-gray-800">{children}</body>
     </html>
   )
 }
