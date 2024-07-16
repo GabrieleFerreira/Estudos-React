e73696d056f77241ed1e5451edb32af9eaa57386
GabrieleFerreira <gabriele.ferreira@ufvjm.edu.br>
2024-07-16 13:50:12 -0300

    layout

--- estudosreact/tailwind.config.ts
+++ estudosreact/tailwind.config.ts
@@ -13,7 +13,7 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
         'custom-bg': "url('/react.jpeg')",
-        'custom-exemp': "url('/ensinando.png')",
+        'custom-exemp': "url('/quadros.png')",
       },
     },
   },
