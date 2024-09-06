7380f4ca9b5707d9998026c5e191d2b0f2183373
GabrieleFerreira <gabriele.ferreira@ufvjm.edu.br>
2024-09-05 21:42:54 -0300

    criação do input para uso do useRef

---
+++ estudosreact/src/app/exemUseImperativeHandle/Input.tsx
@@ -0,0 +1,27 @@
+'use client'
+
+import React, { forwardRef, InputHTMLAttributes } from 'react'
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  name: string
+  label: string
+}
+
+const Input: React.ForwardRefRenderFunction<HTMLInputElement, InputProps> = (
+  { label, name, ...rest },
+  ref
+) => {
+  return (
+    <div className="flex flex-col">
+      <label
+        htmlFor={name}
+        className="flex flex-col justify-center items-center"
+      >
+        {label}
+        <input {...rest} ref={ref} />
+      </label>
+    </div>
+  )
+}
+
+export default forwardRef(Input)
